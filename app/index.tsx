fcc84956edc7c4fc0a516dd1bfd57ff91112036d
wallaceholanda <wallace.holanda@hotmail.com>
2025-08-14 09:30:06 -0300

    feat: create index page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { Image, StyleSheet, Text, TouchableOpacity } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <LinearGradient
+      colors={["#00457D", "#05051F"]}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <Text>Weather App</Text>
+      <Image source={require("../assets/images/climate.png")} />
+      <Text>Seja Bem-Vindo!</Text>
+      <TouchableOpacity>
+        <Text>Entrar</Text>
+      </TouchableOpacity>
+
+    </LinearGradient>
+
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+  },
+});
