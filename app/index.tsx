9ddfaec61ab2bb67e4f9e8dd8cdf4aadaaea33a7
wallaceholanda <wallace.holanda@hotmail.com>
2025-08-14 09:50:10 -0300

    feat: add index page style

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import MaterialIcon from '@expo/vector-icons/MaterialIcons';
 import { LinearGradient } from "expo-linear-gradient";
 import { Image, StyleSheet, Text, TouchableOpacity } from "react-native";
 
@@ -7,13 +8,17 @@ export default function Index() {
       colors={["#00457D", "#05051F"]}
       style={styles.container}
     >
-      <Text>Weather App</Text>
+      <Text style={styles.appTitle}>WeatherApp</Text>
       <Image source={require("../assets/images/climate.png")} />
-      <Text>Seja Bem-Vindo!</Text>
-      <TouchableOpacity>
-        <Text>Entrar</Text>
+      <Text style={styles.text}>Seja Bem-Vindo!</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Entrar</Text>
+        <MaterialIcon
+          size={24}
+          color={'#01080E'}
+          name='arrow-forward'
+        />
       </TouchableOpacity>
-
     </LinearGradient>
 
   );
@@ -22,6 +27,34 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
+    gap: 64,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 80,
+    paddingHorizontal: 32,
+  },
+  appTitle: {
+    fontSize: 26,
+    color: '#FFF',
+    fontWeight: '500',
+  },
+  text: {
+    fontSize: 24,
+    color: '#FFF',
+  },
+  button: {
+    gap: 8,
+    height: 40,
+    width: '100%',
+    borderRadius: 32,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#7693FF',
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#01080E',
   },
 });
